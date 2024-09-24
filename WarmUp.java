fb58b7ffce7532ecdadad31143fd1a2c0876af29
Areyes116 <adrian.reyes2006@icloud.com>
2024-09-24 10:47:36 -0700

    Arise

--- WarmUp.java
+++ WarmUp.java
@@ -3,9 +3,20 @@ public class WarmUp
   public static void main(String[] args)
   {
     // test your methods here
+    boolean var1 = isEven(4);
+    System.out.println(var1);
+
+    boolean var2 = isDivisable(9, 5);
+    System.out.println(var2);
   }
 
   // write your methods below
-
-  
+    public static boolean isEven(int n)
+    {
+       return (n % 2 == 0);
+    }
+  public static boolean isDivisable(int a, int b)
+  {
+    return (a % b == 0);
+  }
 }
